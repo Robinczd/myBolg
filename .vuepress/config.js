2867bfc5502bbb16a80fd36d8ca13d234bb28243
czd <596860296@qq.com>
2023-03-13 00:30:36 +0800

    feat(1)

---
+++ .vuepress/config.js
@@ -0,0 +1,30 @@
+const nav = require('./config/nav')
+const sidebar = require('./config/sidebar')
+// const plugins = require('./config/plugins')
+// const markdown = require('./config/markdown')
+module.exports = {
+    base:"/vuepress-starter/",
+    title:"大数据知识全栈",
+    description:"个人笔记，费时费力，转载请注明出处",
+    head:[
+        ['link',{ rel:'icon',href:'/dds.ico'} ],
+        ['meta',{ name:'author',content:'瑶'} ],
+        ['meta',{ name:'keywords',content:'大数据,大数据知识,Flink,spark,hive,hbase,redis,AI,数据湖'}]
+    ],
+    // plugins,
+    themeConfig: {
+        docsRepo: "stibel/cpp-notes",
+        editLinks: true,
+        sidebarDepth: 0,
+        locales: {
+            "/": {
+                label: "简体中文",
+                selectText: "Languages",
+                editLinkText: "在 GitHub 上编辑此页",
+                lastUpdated: "上次更新",
+                nav,
+                sidebar,
+            }
+        }
+    }
+}
---
+++ .vuepress/config/nav.js
@@ -0,0 +1,235 @@
+module.exports =
+    [
+        {
+            text: '面试',
+            items: [
+                {
+                    text: '编程基础知识考核',
+                    items: [
+                        { text: 'java', link: '/md/c++/basic/c++-basic-use.md' },
+                        { text: 'Jvm', link: '/md/c++/basic/c++-basic-use.md' },
+                        { text: '数据结构与算法', link: '/md/c++/basic/c++-basic-use.md' }
+                    ]
+                },
+                {
+                    text: '大数据基础知识考核',
+                    items: [
+                        { text: 'linux', link: '/md/c++/function/c++-function-Linux_Hook.md' },
+                        { text: '组件原理', link: '/md/c++/function/c++-function-Linux_Hook.md' },
+                        { text: '项目经验', link: '/md/c++/function/c++-function-Linux_Hook.md' }
+                    ]
+                },
+                {
+                    text: '通用能力考核',
+                    items: [
+                        { text: '表达能力', link: '/md/c++/tips/c++-tips-copy-on-write.md' },
+                        { text: '逻辑思维', link: '/md/c++/tips/c++-tips-copy-on-write.md' },
+                        { text: '性格', link: '/md/c++/tips/c++-tips-copy-on-write.md' },
+
+                    ]
+                }
+            ]
+        },
+        {
+            text: '导读',
+            items: [
+                {
+                    text: '我的经历',
+                    items: [
+                        { text: 'java', link: '/md/c++/basic/c++-basic-use.md' }
+                    ]
+                },
+                {
+                    text: '适合人群',
+                    items: [
+                        { text: '函数学习', link: '/md/c++/function/c++-function-Linux_Hook.md' }
+                    ]
+                },
+                {
+                    text: '建议',
+                    items: [
+                        { text: '知识点', link: '/md/c++/tips/c++-tips-copy-on-write.md' }
+                    ]
+                }
+            ]
+        },
+        {
+            text: '初识大数据',
+            items: [
+                { text: '核心思想', link: '' },
+                { text: '发展与应用', link: '/md/algorithm/distribute/alg-distribute-raft.md' },
+                { text: 'hello bigdata', link: '/md/algorithm/distribute/alg-distribute-raft.md' },
+            ]
+        },
+        {
+            text: '编程基础',
+            items: [
+                { text: 'java', link: '/md/basics/java/java.md' },
+                { text: 'scala', link: '/md/basics/scala/scala.md' },
+                { text: 'Jvm', link: '/md/basics/jvm/jvm.md' },
+                { text: '数据结构', link: '/md/basics/structure/structure.md' },
+                { text: '算法', link: '/md/basics/algorithm/algorithm.md' },
+            ]
+        },
+        {
+            text: '组件简介',
+            items: [
+                {
+                    text: '数据源',
+                    items: [
+                        { text: '数据库原理', link: '/md/database/base/db-work-theory.md' },
+                        { text: 'SQL语言', link: '/md/database/sql/sql-overview.md' }
+                    ]
+                },
+                {
+                    text: '接入层',
+                    items: [
+                        { text: 'MySQL', link: '/md/database/mysql/mysql-overview.md' }
+                    ]
+                },
+                {
+                    text: '存储层',
+                    items: [
+                        { text: 'Redis', link: '/md/database/redis/basic/redis-basic-concept.md' },
+                        { text: 'ElasticSearch', link: '/md/database/es/es-install.md' }
+                    ]
+                },
+                {
+                    text: '计算层',
+                    items: [
+                        { text: 'Redis', link: '/md/database/redis/basic/redis-basic-concept.md' },
+                        { text: 'ElasticSearch', link: '/md/database/es/es-install.md' }
+                    ]
+                },
+                {
+                    text: '调度',
+                    items: [
+                        { text: 'Redis', link: '/md/database/redis/basic/redis-basic-concept.md' },
+                        { text: 'ElasticSearch', link: '/md/database/es/es-install.md' }
+                    ]
+                },
+                {
+                    text: '即席',
+                    items: [
+                        { text: 'Redis', link: '/md/database/redis/basic/redis-basic-concept.md' },
+                        { text: 'ElasticSearch', link: '/md/database/es/es-install.md' }
+                    ]
+                },
+                {
+                    text: '监控',
+                    items: [
+                        { text: 'Redis', link: '/md/database/redis/basic/redis-basic-concept.md' },
+                        { text: 'ElasticSearch', link: '/md/database/es/es-install.md' }
+                    ]
+                }
+            ]
+        },
+        {
+            text: '数据中台',
+            items: [
+                { text: '数据结构与算法', link: '/md/programming/data-structure/data-structure-overview.md' },
+                { text: '计算机网络', link: '/md/programming/network/network-physical.md' },
+                { text: '操作系统', link: '/md/programming/system/doc.md' },
+                { text: '设计模式', link: '/md/programming/designpattern/designpattern-overview' }
+            ]
+        },
+        {
+            text: '源码分析',
+            items: [
+                { text: '开发 - 常用库', link: '/md/develop/library/library-cppcheck.md' },
+                { text: '开发 - 工具安装', link: '/md/develop/tools/tools-install.md' },
+            ]
+        },
+        {
+            text: '进阶',
+            items: [
+                { text: '开发 - 常用库', link: '/md/develop/library/library-cppcheck.md' },
+                { text: '开发 - 工具安装', link: '/md/develop/tools/tools-install.md' },
+            ]
+        },
+        {
+            text: '项目',
+            items: [
+                {
+                    text: '开源项目',
+                    items: [
+                        { text: '开源协程库 libco', link: '/md/project/opensource/libco/libco.md' },
+                        { text: 'buttonrpc', link: '/md/project/opensource/buttonrpc/buttonrpc-basic.md' },
+                        { text: 'Tars', link: '/md/project/opensource/tars/tars-brief.md' },
+                        { text: 'Redis实战hmdp', link: '/md/project/opensource/hmdp/hmdp-overview.md' }
+                    ]
+                },
+                {
+                    text: '学习项目',
+                    items: [
+                        { text: 'SSM框架整合', link: '/md/project/study/ssm/spring/spring-basic.md' },
+                    ]
+                },
+                {
+                    text: '个人github项目',
+                    items: [
+                        { text: 'build_lib', link: '/md/project/person/build_lib/build_lib.md' }
+                    ]
+                },
+                {
+                    text: '项目开发',
+                    items: [
+                        { text: '整体开发流程', link: '/md/project/develop/develop-process.md' }
+                    ]
+                }
+            ]
+        },
+        {
+            text: '部署',
+            items: [
+                {
+                    text: '八股文',
+                    items: [
+                        { text: '八股文 详解', link: '/md/interview/knowledge/cplusplus-knowledge.md' }
+                    ]
+                },
+                {
+                    text: 'LeetCode编程题',
+                    items: [
+                        { text: '数据结构', link: '/md/interview/code/data-structure/data-structure-overview.md' },
+                        { text: '刷题套路', link: '/md/interview/code/question-routine/question-routine-backtrack.md' },
+                        { text: '题型分类', link: '/md/interview/code/question-type/question-type-array.md' }
+                    ]
+                }
+            ]
+        },
+        {
+            text: '读书笔记',
+            items: [
+                {
+                    text: '书籍推荐',
+                    items: [
+                        { text: '推荐书籍目录', link: '/md/book-note/book-note-index.md' },
+                    ]
+                },
+                {
+                    text: '读书笔记',
+                    items: [
+                        { text: '深入理解计算机系统', link: '/md/book-note/computersystem/computersystem-chapter-1.md' },
+                        { text: '重构改善既有代码的设计', link: '/md/book-note/refactor/refactor-chapter-1.md' },
+                        { text: 'Effective C++:改善程序与设计的55个具体做法', link: '/md/book-note/effectivecpp/effectivecpp-chapter-2-1.md' },
+                        { text: 'STL源码剖析', link: '/md/book-note/stlsource/stlsource-chapter-1.md' },
+                        { text: 'Linux多线程服务端编程——使用muduo C++ 网络库', link: '/md/book-note/muduo/muduo-chapter-1.md' },
+                        { text: '大型网站技术架构——核心原理与案例分析', link: '/md/book-note/websiteTechArch/websiteTechArch-index.md' },
+                        { text: '代码大全2', link: '/md/book-note/codeComplete/codeComplete-chapter-1.md' }
+                    ]
+                }
+            ]
+        },
+        {
+            text: '成长',
+            items: [
+                { text: '个人', link: '/md/grow/person/person-better-than-others.md' },
+                { text: '产品', link: '/md/grow/product/product.md' },
+                { text: '团队', link: '/md/grow/team/team-system-arch.md' },
+            ]
+        },
+        {
+            text: '关于', link: '/md/about/me/about-me.md'
+        }
+    ]
